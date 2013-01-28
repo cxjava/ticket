c3cd81333a51a5a8d777b8c241a2134b9947ee24
cxjava <cxjava@gmail.com>
2013-01-28 15:01:20 +0800

    how make

---
+++ src/test/java/com/cxjava/ticket/orc/OCRMake.java
@@ -0,0 +1,56 @@
+package com.cxjava.ticket.orc;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+
+/**
+ * 产生字符特征库
+ * 
+ * @author Maty Chen
+ * @date 2013-1-28下午2:37:07
+ */
+public class OCRMake {
+	private static String URL = "http://dynamic.12306.cn/otsweb/passCodeAction.do?rand=sjrand";
+	private static String SAVE_PATH = "D:\\05_Document\\Downloads\\12306\\";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		save();
+		// 可以先筛选质量好的图片，再打印
+		print();
+	}
+
+	/**
+	 * 打印点阵
+	 */
+	private static void print() {
+		PrintImageDotmap.allImageDot(SAVE_PATH);
+	}
+
+	/**
+	 * 获取图片
+	 */
+	private static void save() {
+		try {
+			for (int i = 1; i < 50; i++) {
+				InputStream input = new URL(URL + "&" + Math.random()).openStream();
+				BufferedImage bufferedImage = ImageIO.read(input);
+				// String code = OCR.imageToString(bufferedImage);
+				// LOG.debug("code : {}.", code);
+				File file = new File(SAVE_PATH + i + ".png");
+				ImageIO.write(bufferedImage, "png", file);
+			}
+		} catch (IOException e) {
+		}
+
+	}
+
+}
---
+++ src/test/java/com/cxjava/ticket/orc/PrintImageDotmap.java
@@ -0,0 +1,81 @@
+package com.cxjava.ticket.orc;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author leeass
+ * 
+ */
+public class PrintImageDotmap {
+	private static final Logger LOG = LoggerFactory.getLogger(PrintImageDotmap.class);
+	public static void printImage(File imgfile) {
+		BufferedImage bi;
+		try {
+			bi = ImageIO.read(imgfile);
+			int[][] data = bitmap2DotArray(bi);
+
+			StringBuffer sb = new StringBuffer();
+			sb.append("{");
+			for (int i = 0; i < data.length; i++) {
+				sb.append("\r\n{");
+				for (int j = 0; j < data[0].length; j++) {
+					sb.append(data[i][j]).append(",");
+				}
+				if (i == data.length - 1) {
+					sb.append("}");
+				} else {
+					sb.append("},");
+				}
+			}
+			sb.append("\r\n}\r\n");
+			LOG.info(sb.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void allImageDot(String folder) {
+		File dir = new File(folder);
+		if (dir.isDirectory()) {
+			File[] files = dir.listFiles();
+			for (File imgfile : files) {
+				try {
+					LOG.info(imgfile.getName());
+					printImage(imgfile);
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private static int[][] bitmap2DotArray(BufferedImage buffImage) {
+		int[][] array = new int[buffImage.getHeight()][buffImage.getWidth()];
+
+		for (int i = 0; i < buffImage.getHeight(); i++) {
+			for (int j = 0; j < buffImage.getWidth(); j++) {
+
+				if (170 < getColorWeight(new Color(buffImage.getRGB(j, i)))) {
+					array[i][j] = 0;
+				} else {
+					array[i][j] = 1;
+				}
+			}
+		}
+		return array;
+	}
+
+	private static int getColorWeight(Color color) {
+		return (int) color.getRed() * 19595 + (int) color.getGreen() * 38469 + (int) color.getBlue() * 7472 >> 16;
+	}
+
+}
